0ffcc94b239bfda029849ffd651f93add04e71e2
Inocêncio <inocencio.cardoso@iatec.com>
2020-04-25 16:10:36 -0300

    added custom pipe

--- pipes/src/app/app.module.ts
+++ pipes/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ExemplosPipesComponent } from './exemplos-pipes/exemplos-pipes.component';
+import { CamelCasePipe } from './camel-case.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ExemplosPipesComponent
+    ExemplosPipesComponent,
+    CamelCasePipe
   ],
   imports: [
     BrowserModule,
