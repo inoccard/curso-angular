eb75cebe1eeadc265bb6328e38f3ee3c0c99decf
Inocêncio <inocencio.cardoso@iatec.com>
2020-04-25 16:25:45 -0300

    created custom pipe

--- pipes/src/app/camel-case.pipe.ts
+++ pipes/src/app/camel-case.pipe.ts
@@ -5,8 +5,18 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class CamelCasePipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: any, args?: any): any {
+    let values = value.split(' ')
+    let result = '';
+
+    values.forEach((v: string) => {
+      result += `${this.capitalize(v)} `;
+    });
+    return result;
+  }
+
+  capitalize(value: string) {
+    return value.substr(0, 1).toUpperCase() + value.substr(1).toLowerCase();
   }
 
 }
