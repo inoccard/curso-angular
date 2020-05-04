512e6dfd766499513b1bd1627c28c87e454ce882
Inocêncio <inocencio.cardoso@iatec.com>
2020-05-03 22:09:30 -0300

    adicionado pipe assíncrono

--- pipes/src/app/exemplos-pipes/exemplos-pipes.component.ts
+++ pipes/src/app/exemplos-pipes/exemplos-pipes.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Pipe } from '@angular/core';
+import { Observable, interval } from 'rxjs';
+import { timeout } from 'rxjs/operators';
 
 @Component({
   selector: 'app-exemplos-pipes',
@@ -15,7 +17,18 @@ export class ExemplosPipesComponent implements OnInit {
     date: new Date(2017, 6, 26),
     url: 'https://ler.amazon.com.br/kp/embed?asin=B07H4WYN5M&preview=newtab&linkCode=kpe&ref_=cm_sw_r_kb_dp_VjiPEb7J4Z6HT'
   }
-  constructor() { }
+
+  /**
+   * Promessa
+   * 1º opção de fazer pipes assyncrono 
+   */
+  valorAsync = new Promise((resolve) => {
+    setTimeout(() => resolve('valor assíncrono'), 2000);
+  });
+
+  constructor() { 
+    
+  }
 
   ngOnInit(): void {
   }
